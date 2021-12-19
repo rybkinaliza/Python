5b424944ce065a36de2266564e59fdfbddcd8979
Liza Rybkina <rybkina.eg@phystech.edu@>
2021-12-19 12:06:36 +0300

    new

--- lab_6/lab_6_num_2.py
+++ lab_6/lab_6_num_2.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[33]:
+# In[23]:
 
 
 class Rational:
     def __init__(self, numerator, denominator):
         self.numerator = numerator
         self.denominator = denominator
+        if self.denominator == 0:
+            raise ValueError
         
     def __str__(self):
         return f'{self.numerator}/{self.denominator}'
@@ -52,9 +54,28 @@ class Rational:
     
     def from_string(string):
         all_num = string.split('/')
-        n = all_num[0]
-        m = all_num[1]
+        n = int(all_num[0])
+        m = int(all_num[1])
         return Rational(n, m)
+    
+    def __eq__(self, other):
+        if self.numerator !=0 and other.numerator != 0:
+            if self.numerator > other.numerator:
+                z = self.numerator / other.numerator
+            else:
+                z = other.numerator / self.numerator
+            if self.denominator > other.denominator:
+                k = self.denominator / other.denominator
+            else:
+                k = other.denominator / self.denominator
+            if z == k:
+                return True
+            else:
+                return False
+        elif self.numerator == other.numerator and other.numerator == 0:
+            return True
+        else:
+            return False
 
 
 def test_operations():
@@ -68,11 +89,17 @@ def test_cast_to_float():
 
 
 def test_parse_from_string():
-    assert Rational.from_string('3/4') == '3/4'
+    assert Rational.from_string('3/4') == Rational(3, 4)
 
 
 if __name__ == '__main__':
     test_operations()
     test_cast_to_float()
     test_parse_from_string()
+
+
+# In[ ]:
+
+
+
 
